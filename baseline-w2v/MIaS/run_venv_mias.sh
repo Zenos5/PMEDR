2a4e61140fe48581f413f32605388b1d29bdfddf
Angel Wheelwright <angelwhwrt@gmail.com>
2024-04-25 16:38:26 -0600

    Added all appendage methods for MIaS to attempt training

---
+++ baseline-w2v/MIaS/run_venv_mias.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00   # walltime.  hours:minutes:seconds
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=1
+#SBATCH --mem=80G   # 164G memory per CPU core
+#SBATCH --mail-user=aw742@byu.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --qos=cs
+#SBATCH --partition=cs
+
+# some helpful debugging options
+set -e
+set -u
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load python/3.11 jdk maven/3.6
+
+cd $HOME/PMEDR/baseline-w2v/MIaS/
+
+cd MathMLCan/
+mvn clean install
+cd ..
+
+cd MathMLUnificator/
+mvn clean install
+cd ..
+
+cd MIaSMath/
+mvn clean install
+cd ..
+
+cd MIaS/
+mvn clean install
+cd ..
+
+cd WebMIaS/
+mvn clean install
+cd ..
